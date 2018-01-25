319a88cc972d09359b932fa155f6216b5b1956a4
yorshreynoso <yorsh.reynoso@gmail.com>
2018-01-25 17:29:01 -0600

    program finished

--- public_html/Javascript/countDownTimer.js
+++ public_html/Javascript/countDownTimer.js
@@ -1,12 +1,9 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 26, 2018 9:00:00").getTime();
-
-
+var countDownDate = new Date("May 29, 2018 9:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(countDown, 1000);
-
+var interval = setInterval(countDown, 1000);
 
 function countDown() {
 
@@ -14,8 +11,10 @@ function countDown() {
     var difference = (countDownDate - time);
     //var difference = "19270495396";
 
+    //Convert time in Day, Hours, Minutes, Seconds
     var day = Math.floor(difference / (1000 * 60 * 60 * 24));
     console.log("day " + day);
+
     var hour = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     console.log("hour " + hour);
 
@@ -27,10 +26,15 @@ function countDown() {
 
     printScreen(day, hour, minute, second);
 
+    if (difference < 0) {
+        clearInterval(interval);            //Stop Timer
+        document.getElementById("counter").innerHTML = ("Time is up!!");
+    }
 }
 
+//print in screen
 function printScreen(day, hour, minute, second) {
     document.getElementById("counter").innerHTML = (day + " Days, " + hour + " Hours, "
             + minute + " Minutes, " + second + " Seconds");
 }
-   
+
